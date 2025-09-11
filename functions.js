3318515c9eb7196f415826a7b8242b949a0d9530
ryan_roeling <ryan_roeling@tamu.edu>
2025-09-10 22:26:56 -0500

    design overhaul and picture inclusions

--- functions.js
+++ functions.js
@@ -1,14 +1,24 @@
-let isDefault = true;
+function applyTheme(isDefault) {
+  const themeLink = document.getElementById('themeStylesheet');
+  const profileImage = document.getElementById('profileImage');
+
+  if (isDefault) {
+    themeLink.href = "style.css";
+    if (profileImage) profileImage.src = "images/profile-light.jpg";
+  } else {
+    themeLink.href = "alternative.css";
+    if (profileImage) profileImage.src = "images/profile-dark.jpg";
+  }
+}
 
 function button1() {
-   const themeLink = document.getElementById('themeStylesheet');
-
-   if(isDefault) {
-    themeLink.href = "alternative.css";
-   }
-   else {
-    themeLink.href = 'style.css';
-   }
-
-   isDefault = !isDefault;
+  let isDefault = localStorage.getItem("theme") === "default";
+  isDefault = !isDefault; 
+  localStorage.setItem("theme", isDefault ? "default" : "alternate");
+  applyTheme(isDefault);
 }
+
+window.onload = () => {
+  const saved = localStorage.getItem("theme");
+  applyTheme(saved !== "alternate"); 
+};
